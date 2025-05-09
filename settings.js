e46eca54993f9d0539979b638b2004eb5f59fffd
MEMORABLE-UI <glendenosabutey5@gmail.com>
2025-05-09 17:59:33 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'glenden', // Your name
+  botOwner: 'knight bot', // Your name
   ownerNumber: '0576830078', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
